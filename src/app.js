985c78bad769c739239a98c017ff7627c5c524e9
Brandon Avant <avant.brandon@gmail.com>
2023-07-08 18:30:27 -0500

    Fixing node app error.

--- src/app.js
+++ src/app.js
@@ -2,5 +2,5 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (res) => res.send("The app is up and running!"));
+app.get("/", (req, res) => res.send("The app is up and running!"));
 app.listen(port, () => console.log(`The app is listening on port ${port}.`));
