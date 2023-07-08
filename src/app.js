dd5184474612d2e1708fb1e0b13084813c2930eb
Brandon Avant <avant.brandon@gmail.com>
2023-07-08 18:04:16 -0500

    Making another change to test build/deploy trigger.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', res => res.send("The app is up and running!"));
+app.get("/", (res) => res.send("The app is up and running!"));
 app.listen(port, () => console.log(`The app is listening on port ${port}.`));
